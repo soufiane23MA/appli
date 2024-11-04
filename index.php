6567ae505f70883ff00ce18423760bc75bade36d
soufiane23Ma <soufiane.elmaarouf@gmail.com>
2024-11-04 07:59:43 +0100

    sollution les 4 premiéres questions

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+
+session_start();
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,15 +13,54 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Ajout Produit</title>
+	<link rel="stylesheet" href="styles/index_style.css">
 </head>
 
 <body>
+
+	<?php
+
+	$qqtGenaral = 0;
+	if (isset($_SESSION['products'])) {
+		foreach ($_SESSION['products'] as $product) {
+			$qqtGenaral += $product['qtt'];
+		}
+	};
+	?>
+
+	<div class="navbare">
+		<div class="navlinks">
+			<ul>
+				<li><a href="index.php">Accueil</a></li>
+				<li><a href="recap.php">Recap</a></li>
+				<li id="total">Total Produits :<?php echo $qqtGenaral; ?> </li>
+			<!-- crée le lien cliquable qui permet de suprimer tou les produit du tableau-->	
+				<li><a href="recap.php?action=clear">Vider Votre Panier</a></li>
+			</ul>
+		</div>
+	</div>
 	<!--création du formulaire qui permettra de renseigner les produits-->
 	<h1>Ajouter un Produit</h1>
+	<?php
+	
+	// Affichage du message de retour
+
+	if (isset($_SESSION['message'])) {
+		echo "<p class='message'> {$_SESSION['message']}</p>";
+		unset($_SESSION['message']);
+		/**
+		 * On supprime le message pour qu'il ne persiste pas
+		 * le problem je n'arrive pas a enlevé le message de retour 
+		 * je ne sais pas pourquoi.mlagré l'utilisation de unset.
+		 */ 
+	};
+	?>
+
 	<!-- l'attribut action permet de désignié la destination de notre formulaire
 	 et la methode nous informe le serveur que les informations sont récupereés
 	 par un formulaire et ne pas via l'URL (methode GET)-->
-	<form action="traitement.php" method="post">
+
+	<form class="form_produit" action="traitement.php" method="post">
 		<p>
 			<label>
 				Nom du produit :
@@ -33,10 +80,12 @@
 			</label>
 		</p>
 		<p>
-			<input type="submit" name="submit" value="Ajouter le produit">
+			<input class="submit" type="submit" name="submit" value="Ajouter le produit">
 		</p>
 
 	</form>
+
+
 
 </body>
 
--- recap.php
+++ recap.php
@@ -5,6 +5,63 @@
  * on commence toujour par cette fonction qui permettra l'affichage de la sesson
  */
 session_start();
+/**
+ * en bas : la condition qui permet de suprimer le produit clické (1 seul produit)
+ * je verifier que le produit existe et que le paramétre 'action' avec comme valeur (supprimer/ delete)
+ *  est dans l'URL reçus par recap.php, via le lien que j'ai crée dans le tableau( index.php )
+ *  je vérifie également que le paramétre 'index' exist et que c'est un entier (# de NULL)
+ */
+
+if (isset($_GET['action']) && $_GET['action'] == 'delete' && isset($_GET['index'])) {
+	$index = (int) $_GET['index'];
+	if (isset($_SESSION['products'][$index])) {
+		unset($_SESSION['products'][$index]); // Supprime le produit
+		$_SESSION['products'] = array_values($_SESSION['products']); // Ré-indexe le tableau
+		$_SESSION['message'] = "Le produit a été supprimé.";
+	}
+};
+/**
+ * la verification de l'action tous supprimé , 
+ * là on suprime tous les produits on utilisant la valeur Clear pour le paramétre de l'URL
+ * et on utilise la fonction (methode) unset avec comme paramétre le tableau des produits de la session 
+ * et on envoie un message pour dire que le panier est vidée.
+ */
+if (isset($_GET["action"]) && $_GET["action"] == "clear") {
+	unset($_SESSION['products']);
+	$_SESSION['message'] = 'Votre Panier est Vide';
+};
+/**
+ * la fonction qui permet d'augmenter la quantité de produits
+ */
+
+if (isset($_GET['action']) && $_GET['action'] == 'increase' && isset($_GET['index'])) {
+	$index = (int)$_GET['index'];
+	if (isset($_SESSION['products'][$index])) {
+		$_SESSION['products'][$index]['qtt']++;
+		$_SESSION['products'][$index]['total'] = $_SESSION['products'][$index]['qtt'] * $_SESSION['products'][$index]['price'];
+		$_SESSION['message'] = "<h3 style='color:green'>Votre produit a été bien rajouté</h3>";
+	}
+};
+/**
+ * la fonction qui permet de réduire la quantité de produit dans le panier
+ */
+if (isset($_GET['action']) && $_GET['action'] == 'decrease' && isset($_GET['index'])) {
+	$index = (int)$_GET['index'];
+	if (isset($_SESSION['products'][$index]) && $_SESSION['products'][$index]['qtt'] > 1) {
+		$_SESSION['products'][$index]['qtt']--;
+		$_SESSION['products'][$index]['total'] = $_SESSION['products'][$index]['qtt'] * $_SESSION['products'][$index]['price'];
+		$_SESSION['message'] = "<h3 style='color:red'> Votre produit a été bien supprimé </h3>.";
+	}
+};
+
+
+$qqtGenaral = 0;
+if (isset($_SESSION['products'])) {
+	foreach ($_SESSION['products'] as $product) {
+		$qqtGenaral += $product['qtt'];
+	}
+};
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,9 +70,23 @@ session_start();
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Récapitulatif des Produits</title>
+	<link rel="stylesheet" href="styles/recap_style.css">
 </head>
 
 <body>
+
+	<div class="navbare">
+		<div class="navlinks">
+			<ul>
+
+				<li><a href="index.php">Accueil</a></li>
+				<li><a href="recap.php">Recap</a></li>
+				<li id="total">Total Produits :<?php echo $qqtGenaral; ?> </li>
+			</ul>
+		</div>
+
+	</div>
+
 	<?php
 	/**
 	 * ici j'ai mis en place une condition  avec un message destiné à l'utilisateur, pour s'assuérer que mon tableau
@@ -24,48 +95,68 @@ session_start();
 	 */
 
 	if (!isset($_SESSION["products"]) || empty($_SESSION['products'])) {
-		echo "<p>Aucun Produits en session...</p>";
+		echo "<h3>Aucun Produits en session...</h3>";
 	} else {
-		echo "<table>",
+		echo "<table class='product-table'>",
 		"<thead>",
 		"<tr>",
 		"<th>#</th>",
+		"<th>poubelle</th>",
 		"<th>Nom</th>",
 		"<th>Prix</th>",
 		"<th>Quantité</th>",
 		"<th>Total</th>",
 		"</tr>",
 		"</thead>",
-		//"</table>",
+
 		"<tbody>";
+
+
 		/**
 		 * initialiser le totalgenaral,et parcourir le tableau de la session avec tous les produits
 		 * 
 		 */
 
 		$totalGeneral = 0;
+		// declarer total des quantitées dans une varibale
+
 		foreach ($_SESSION['products'] as $index => $product) {
 
 			echo "<tr>",
 			"<td>" . $index . "</td>",
+			"<td><a href='recap.php?action=delete&index=$index'>Supprimer</a></td>", //le lien pour suprimer les produits
 			"<td>" . $product['name'] . "</td>",
-			/*"<td>" . $product['price'] . "</td>",*/ "<td>" . number_format($product['price'], 2, ",", "&nbsp;") . "&nbsp;€" . "</td>",
-			"<td>" . $product['qtt'] . "</td>",
-			/*"<td>" . $product['total'] . "</td>",*/ "<td>" . number_format($product['total'], 2, ",", "&nbsp;") . "&nbsp;€" . "</td>",
+			//"<td>" . $product['price'] . "</td>",  comme c'est nombre en utilise le format 
+			"<td>" . number_format($product['price'], 2, ",", "&nbsp;") . "&nbsp;€" . "</td>",
+			"<td>" . "<a href='recap.php?action=decrease&index=$index'>-</a> " . $product['qtt'] . " <a href='recap.php?action=increase&index=$index'>+</a></td>",
+			//"<td>" . $product['total'] . "</td>",
+			"<td>" . number_format($product['total'], 2, ",", "&nbsp;") . "&nbsp;€" . "</td>",
 			"</tr>";
 
 			$totalGeneral += $product['total'];
+			/**
+			 * qqtgenerale va accumuler tous les quantités de la session
+			 * et j 'ai rajouter l'affichage du total des quantité dans le tableau
+			 */
+			$qqtGenaral += $product['qtt'];
 		}
 		echo "<tr>",
-		"<td colspan=4> Total général : </td>",
+		"<td colspan=5> Total général : </td>",
+		//"<td  >".$qqtGenaral."</td>",
 		"<td><strong>" . number_format($totalGeneral, 2, ",", "&nbsp;") . "&nbsp;€</strong></td>",
 		"</tr>",
 		"</tbody>",
-		"</table>";
+		"</table>";;
 	};
-	 
 
 	?>
+
+
+
+
+
+
+
 </body>
 
 </html>
